a9acb49463af223314fccf1cd61875d4386699b2
Loanina <yhololeenko@mail.ru>
2023-09-10 00:28:49 +0300

    FillWordsParsing

--- Assets/App/Scripts/Scenes/SceneFillwords/Features/ProviderLevel/ProviderFillwordLevel.cs
+++ Assets/App/Scripts/Scenes/SceneFillwords/Features/ProviderLevel/ProviderFillwordLevel.cs
@@ -1,64 +1,69 @@
 using System;
-using System.Collections.Generic;
 using App.Scripts.Scenes.SceneFillwords.Features.FillwordModels;
 using UnityEngine;
-using UnityEngine.Windows;
-using System.IO;
 using System.Linq;
 using File = System.IO.File;
-using System.Collections.Generic;
 
 namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
 {
     public class ProviderFillwordLevel : IProviderFillwordLevel
     {
+
         public GridFillWords LoadModel(int index)
         {
             //напиши реализацию не меняя сигнатуру функции
             try
-            {
-                string[] levelsList = File.ReadAllLines("Assets/App/Resources/Fillwords/pack_0.txt");
-                string[] wordsList = File.ReadAllLines("Assets/App/Resources/Fillwords/words_list.txt");
+            { 
+                //string[] levelsList = File.ReadAllLines("Assets/App/Resources/Fillwords/pack_0.txt");
+                //string[] wordsList = File.ReadAllLines("Assets/App/Resources/Fillwords/words_list.txt");
+                var levelsTextAsset = Resources.Load<TextAsset>("Fillwords/pack_0"); //2
+                var wordsTextAsset = Resources.Load<TextAsset>("Fillwords/words_list"); //2
 
-                var levelInfo = levelsList[index-1].Split(' ');
+                // var levelInfo = levelsList.[index-1].Split(' ');
+                string[] levelInfo = levelsTextAsset.text.Split('\n')[index-1].Split(' '); //2
 
-                //словарь номер слова - номера букв слова
-                var levelWords = new Dictionary<int, int[]>();
+                var levelWordsCount = levelInfo.Length / 2;
 
-                for (int i = 0; i < levelInfo.Length / 2; i++)
+                string gridLetters = "";
+                int lettersCount = 0;
+                for (int i = 0; i < levelWordsCount; i++)
                 {
-                    var wordNum = int.Parse(levelInfo[i * 2]);
-                    var wordLettersNums = levelInfo[i * 2 + 1].Split(';').Select(Int32.Parse).ToArray();
-                    levelWords.Add(wordNum, wordLettersNums);
-                }
-
-                //устанавливаем размерность грида
-                Vector2Int gridSize = new Vector2Int(levelWords.Keys.Count, levelWords.Keys.Count);
-                GridFillWords gridFillWords = new GridFillWords(gridSize);
-                
-                //написть как заполняем грид букваи
-                foreach (var key in levelWords.Keys)
-                {
-                    var word = wordsList[key]; 
-                    var letterNums = levelWords[key];
+                    //var word = wordsList[int.Parse(levelInfo[i * 2])];
+                    var word = wordsTextAsset.text.Split('\n')[int.Parse(levelInfo[i * 2])]; //2
                     
+                    var letterNums = levelInfo[i * 2 + 1].Split(';').Select(Int32.Parse).ToList();
                     
-                    //меняем порядок букв в слове
-                    for (int i = 0; i <= letterNums.Length; i++)
+                    //добавляем буквы в нужном порядке
+                    for (int j = 0; j < word.Length; j++)
                     {
-                        CharGridModel letter = new CharGridModel(word[letterNums[i]]);
-                        //занести букву в грид
+                        if (letterNums.IndexOf(j)==-1) 
+                            letterNums.Add(j);
+                        gridLetters += word[letterNums.IndexOf(j)];
                     }
                     
+                    lettersCount += word.Length;
                 }
-
+                
+                //устанавливаем размерность грида
+                var gridSize = (int)Math.Sqrt(lettersCount);
+                GridFillWords gridFillWords = new GridFillWords(new Vector2Int(gridSize,gridSize));
+                
+                int letterIndex = 0;
+                for (int i=0; i < gridSize; i++){
+                    for (int j = 0; j <gridSize; j++)
+                    {
+                        gridFillWords.Set(i,j, new CharGridModel(gridLetters[letterIndex]));
+                        letterIndex++;
+                    }
+                }
+                
+                return gridFillWords;
             }
             catch (Exception e)
             {
                 Debug.Log(e);
+                return null;
             }
-
-            throw new NotImplementedException();
         }
     }
 }
