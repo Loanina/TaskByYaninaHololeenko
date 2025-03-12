0b66b9b906c10d975ef8f21b853645a2928a3c86
Loanina <96134664+Loanina@users.noreply.github.com>
2025-03-12 17:52:48 +0300

    Installer and joystick input

---
+++ Assets/Scripts/Input/JoystickInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Input
+{
+    public class JoystickInput
+    {
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+
+        public void UpdateInput(Vector2 input)
+        {
+            Horizontal = input.x;
+            Vertical = input.y;
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,30 @@
+﻿using Input;
+using Interaction;
+using Player;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        [SerializeField] private Rigidbody playerRb;
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private PlayerConfig playerConfig;
+        [SerializeField] private InteractionConfig interactionConfig;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(playerConfig).AsSingle();
+            Container.BindInstance(interactionConfig).AsSingle();
+            Container.BindInstance(playerRb).AsSingle();
+            Container.BindInstance(playerTransform).AsSingle();
+            Container.BindInstance(playerCamera).AsSingle();
+
+            Container.Bind<JoystickInput>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PlayerMovement>().AsSingle();
+            Container.Bind<InteractionHandler>().AsSingle();
+        }
+    }
+}
