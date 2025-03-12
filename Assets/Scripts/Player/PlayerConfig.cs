6d5f6827cd3d47d514a4aefefcb4ba853d12811e
Loanina <96134664+Loanina@users.noreply.github.com>
2025-03-12 17:51:42 +0300

    Player

---
+++ Assets/Scripts/Player/PlayerConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "PlayerConfig", menuName = "Settings/PlayerConfig")]
+    public class PlayerConfig : ScriptableObject
+    {
+        [Range(0.5f, 10f)] public float MoveSpeed = 3f;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using Input;
+using UnityEngine;
+using Zenject;
+
+namespace Player
+{
+    public class PlayerMovement : ITickable
+    {
+        private readonly PlayerConfig config;
+        private readonly JoystickInput joystickInput;
+        private readonly Rigidbody rb;
+        private readonly Transform transform;
+
+        public PlayerMovement(Rigidbody rb, JoystickInput joystickInput, PlayerConfig config, Transform transform)
+        {
+            this.rb = rb;
+            this.joystickInput = joystickInput;
+            this.config = config;
+            this.transform = transform;
+        }
+
+        public void Tick()
+        {
+            var move = new Vector3(joystickInput.Horizontal, 0, joystickInput.Vertical);
+            rb.velocity = transform.TransformDirection(move) * config.MoveSpeed;
+        }
+    }
+}
