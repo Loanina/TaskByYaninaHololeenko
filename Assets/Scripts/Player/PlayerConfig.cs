3effd66ccf15a4893b5c35471ec86e4ebf09b1d1
Loanina <96134664+Loanina@users.noreply.github.com>
2025-03-13 02:41:08 +0300

    Change speed

--- Assets/Scripts/Player/PlayerConfig.cs
+++ Assets/Scripts/Player/PlayerConfig.cs
@@ -5,7 +5,7 @@ namespace Player
     [CreateAssetMenu(fileName = "PlayerConfig", menuName = "Settings/PlayerConfig")]
     public class PlayerConfig : ScriptableObject
     {
-        [Range(0.5f, 30f)] public float MoveSpeed = 3f;
+        [Range(0.5f, 100f)] public float MoveSpeed = 3f;
         [Range(0f,50f)] public float RotationSmoothing = 15f;
         [Range(60f, 120f)] public float RotationClampY = 80f;
     }
